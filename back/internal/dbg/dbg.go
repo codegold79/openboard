554069e0eafb57e34e5000852c2f1ee43f46957b
codegold79 <codegold79@gmail.com>
2021-05-31 19:56:36 -0700

    debug logger sync.Mutex version
    Signed-off-by: codegold79 <codegold79@gmail.com>

--- back/internal/dbg/dbg.go
+++ back/internal/dbg/dbg.go
@@ -3,46 +3,47 @@ package dbg
 import (
 	"io"
 	"log"
-	"sync/atomic"
+	"sync"
 )
 
-var atomicV atomic.Value
-
+// DbgLog is thread-safe.
 type DbgLog struct {
-	log *log.Logger
+	mutex sync.Mutex
+	log   *log.Logger
 }
 
 func New(out io.Writer) *DbgLog {
 	var dl DbgLog
 
 	if out == nil {
-		atomicV.Store(&dl)
 		return &dl
 	}
 
 	dl = DbgLog{
 		log: log.New(out, "[debug] ", log.Ldate|log.Ltime),
 	}
-	atomicV.Store(&dl)
 	return &dl
 }
 
 func (l *DbgLog) Log(format string, as ...interface{}) {
-	load := atomicV.Load().(*DbgLog)
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
 
-	if load.log == nil {
+	if l.log == nil {
 		return
 	}
 
-	load.log.Printf(format+"\n", as...)
+	l.log.Printf(format+"\n", as...)
 }
 
 func (l *DbgLog) Off() {
+	l.mutex.Lock()
 	l.log = nil
-	atomicV.Store(l)
+	l.mutex.Unlock()
 }
 
 func (l *DbgLog) On(out io.Writer) {
+	l.mutex.Lock()
 	l.log = log.New(out, "[debug] ", log.Ldate|log.Ltime)
-	atomicV.Store(l)
+	l.mutex.Unlock()
 }
